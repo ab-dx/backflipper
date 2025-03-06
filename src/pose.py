904237c98d3e04831e33ba24116d701b3f922520
helix <abhinavdatta1@gmail.com>
2025-03-06 16:44:41 +0530

    Camera locomotion control

--- src/pose.py
+++ src/pose.py
@@ -13,6 +13,7 @@ vid1 = cv2.VideoCapture(config['video']['src'])
 SCALE=config['window']['scale']
 WIDTH = config['window']['width']
 HEIGHT = config['window']['height']
+STEP_SIZE = config['camera']['step_size']
 camera_start = Vector3(*config['camera']['start'])
 camera_end = Vector3(*config['camera']['end'])
 camera_perpendicular = Vector3(*config['camera']['perpendicular'])
@@ -47,13 +48,25 @@ def main():
                     p.y *= SCALE
                     p.z *= SCALE
                     point_map[i] = Vector3(p.x,p.y,p.z)
-                draw_line_3d(Vector3(-100,0,0), Vector3(100,0,0), config['window']['x-color'])
-                draw_line_3d(Vector3(0,-100,0), Vector3(0,100,0), config['window']['y-color'])
-                draw_line_3d(Vector3(0,0,-100), Vector3(0,0,100), config['window']['z-color'])
+                draw_line_3d(Vector3(-1000,0,0), Vector3(1000,0,0), config['window']['x-color'])
+                draw_line_3d(Vector3(0,-1000,0), Vector3(0,1000,0), config['window']['y-color'])
+                draw_line_3d(Vector3(0,0,-1000), Vector3(0,0,1000), config['window']['z-color'])
                 for k in config['ragdoll']['connections']:
                     draw_line_3d(point_map[k[0]],point_map[k[1]],config['ragdoll']['primary-color'])
             end_mode_3d()
             end_drawing()
+            if is_key_pressed(KeyboardKey.KEY_W):
+                camera.position.z += STEP_SIZE
+            if is_key_pressed(KeyboardKey.KEY_A):
+                camera.position.x -= STEP_SIZE
+            if is_key_pressed(KeyboardKey.KEY_S):
+                camera.position.z -= STEP_SIZE
+            if is_key_pressed(KeyboardKey.KEY_D):
+                camera.position.x += STEP_SIZE
+            if is_key_pressed(KeyboardKey.KEY_DOWN):
+                camera.position.y -= STEP_SIZE
+            if is_key_pressed(KeyboardKey.KEY_UP):
+                camera.position.y += STEP_SIZE
             if cv2.waitKey(1) == ord('q'):
                 break
             cv2.destroyAllWindows
